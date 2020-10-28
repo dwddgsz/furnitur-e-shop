39246112060099766ba5e3e648465b2c5ab34f75
dwddgsz <dwddgsz@gmail.com>
2020-10-28 17:20:19 +0100

    add:database

---
+++ src/data/products.js
@@ -0,0 +1,104 @@
+import bookshelf from '../images/bookshelf.jpg';
+import chandelier from '../images/chandalier.jpg';
+import couchWithPillows from '../images/couch-with-pillows.jpg';
+import darkChair from '../images/dark-chair.jpg';
+import darkCouch from '../images/dark-couch.jpg';
+import modernChair from '../images/modern-chair.jpg';
+import pillows from '../images/pillows.jpg';
+import smallCouch from '../images/small-couch.jpg';
+import spaciousCouch from '../images/spacious-couch.jpg';
+import wardrobe from '../images/wardrobe.jpg';
+import lamp from '../images/lamp.jpg';
+import frame from '../images/frame.jpg';
+
+
+
+const products = [
+    {
+        id:1,
+        name: 'bookshelf',
+        price: 100,
+        img: bookshelf,
+        inCart: false,
+    },
+    {
+        id:2,
+        name: 'chandelier',
+        price: 120,
+        img: chandelier,
+        inCart: false,
+    },
+    {
+        id:3,
+        name: 'grey couch',
+        price: 300,
+        img: couchWithPillows,
+        inCart: false,
+    },
+    {
+        id:4,
+        name: 'dark chair',
+        price: 80,
+        img: darkChair,
+        inCart: false,
+    },
+    {
+        id:5,
+        name: 'dark couch',
+        price: 330,
+        img: darkCouch,
+        inCart: false,
+    },
+    {
+        id:6,
+        name: 'mordern chair',
+        price: 110,
+        img: modernChair,
+        inCart: false,
+    },
+    {
+        id:7,
+        name: 'pillows',
+        price: 60,
+        img: pillows,
+        inCart: false,
+    },
+    {
+        id:8,
+        name: 'small couch',
+        price: 260,
+        img: smallCouch,
+        inCart: false,
+    },
+    {
+        id:9,
+        name: 'spacious couch',
+        price: 400,
+        img: spaciousCouch,
+        inCart: false,
+    },
+    {
+        id:10,
+        name: 'wardrobe',
+        price: 500,
+        img: wardrobe,
+        inCart: false,
+    },
+    {
+        id:11,
+        name: 'lamp',
+        price: 70,
+        img: lamp,
+        inCart: false,
+    },
+    {
+        id:12,
+        name: 'frame',
+        price: 45,
+        img: frame,
+        inCart: false,
+    },
+]
+
+
+export default products;
