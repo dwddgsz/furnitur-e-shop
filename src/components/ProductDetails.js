6694531c696678864ca7d63dfe871aa319fdd98b
dwddgsz <dwddgsz@gmail.com>
2020-10-29 16:45:22 +0100

    add:add redux store to ProductDetails

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import products from '../data/products';
 import {Link} from 'react-router-dom';
+import {addProduct} from '../actions';
+import {connect} from 'react-redux';
 
 
 const DetailsWrapper = styled.article`
@@ -37,11 +38,16 @@ h2 {
             padding:15px 0;
         }
         .details-description {
+            display:block;
+            max-width:700px;
+            padding:20px 0;
             font-size:1.4rem;
+            margin:0 auto;
         }
         .details-value-container {
             padding:10px 0;
             font-size:2.4rem;
+            padding-bottom:20px;
         }
         .details-value {
             margin-right:2px;
@@ -91,14 +97,17 @@ h2 {
 
 `
 
-const ProductDetails = (props) => {
-    const {name,price,img,inCart} = products[props.match.params.id - 1];
+const ProductDetails = ({products,match,addProduct}) => {
+    const product =  products[match.params.id - 1];
+    const {name,price,img,inCart} = products[match.params.id - 1];
     const renderButton = () => {
         if (inCart === false) {
-            return <button className="details-button-add">Add to cart</button>
+            return <button onClick={()=>{
+                addProduct(product)
+            }} className="details-button-add">Add to cart</button>
         }
         else {
-            return <button className="details-button-add" disabled>Add to cart</button>
+            return <button className="details-button-add" disabled='true'>In cart</button>
         }
     }
     return (
@@ -111,7 +120,7 @@ const ProductDetails = (props) => {
 
                 <div className="details-content-container">
                     <h4 className="details-heading">{name}</h4>
-                    <p className="details-description">Lorem lorem reomsad faovmsd xzjidsad Lorem lorem reomsad faovmsd xzjidsad Lorem lorem reomsad faovmsd xzjidsad Lorem lorem reomsad faovmsd xzjidsad</p>
+                    <p className="details-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et nunc sit amet turpis blandit faucibus. Duis molestie erat vitae interdum cursus. Donec at dolor eu enim placerat rhoncus. Curabitur consequat massa in lacus convallis eleifend. Praesent nec eros pretium, scelerisque odio eget, imperdiet urna. In consequat feugiat efficitur. Sed diam diam, consectetur sed tempus sed, vehicula nec orci. Curabitur feugiat nec enim ut rhoncus. Quisque id tincidunt urna.</p>
                     <p className="details-value-container"><span className="details-value">{price}</span>$</p>
                     <div className="details-buttons-container">
                     {renderButton()}
@@ -129,4 +138,10 @@ const ProductDetails = (props) => {
     )
 }
 
-export default ProductDetails;
+const mapStateToProps = (state) => {
+    return {
+        products: state.cart.products,
+    }
+}
+
+export default connect(mapStateToProps,{addProduct})(ProductDetails);
