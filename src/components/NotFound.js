f8f452ab21ac38bef199b0f3aa26331ce3e4e7ce
dwddgsz <dwddgsz@gmail.com>
2020-10-29 16:44:17 +0100

    add:NotFound page styling

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const NotFoundWrapper = styled.article`
+max-width:1200px;
+margin:0 auto;
+.not-found-heading {
+    padding:50px 0;
+    font-size:2.6rem;
+    text-align:center;
+    text-transform:capitalize;
+}
+
+`
+
 
 const NotFound = () => {
     return (
-        <div>
-            not
-        </div>
+        <NotFoundWrapper>
+            <h3 className="not-found-heading">page not found</h3>
+        </NotFoundWrapper>
     )
 }
 
