ab6ac986d6c5d7cff8cbb19d984a05d39900b302
dwddgsz <dwddgsz@gmail.com>
2020-10-29 16:10:48 +0100

    add:state to local storage

--- src/index.js
+++ src/index.js
@@ -6,7 +6,37 @@ import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const loadStore = () => {
+  try {
+  const serializedState = localStorage.getItem('state');
+  if (serializedState === null) {
+    return undefined;
+  }
+  return JSON.parse(serializedState);
+}
+catch(err) {
+  return undefined;
+}
+}
+
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state',serializedState);
+  }
+  catch(err) {
+    console.log('error');
+  }
+  
+}
+
+const persistedState = loadStore();
+
+const store = createStore(rootReducer,persistedState,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+store.subscribe(()=>{
+  saveState(store.getState())
+})
 
 ReactDOM.render(
   <Provider store={store}>
